216927251166dd6fc6b53d500e23ae4c05741456
kazu08271204 <jddpd083@gmail.com>
2019-08-09 05:17:17 +0000

    「基本時間」&「指定勤務時間」

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  
+  # 勤怠基本情報を指定のフォーマットで返します。
+  
+  def format_basic_info(time)
+    format("%.2f", ((time.hour * 60) + time.min) / 60.0)
+    #戻り値となる文字列書式を設定しフォーマットするための値を計算している。
+  end
+  
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ApplicationRecord
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 } # 存在性、文字数の検証
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # メールアドレスの正規表現
   validates :email, presence: true, length: { maximum: 100 }, # 存 在性、文字数の検証
                     format: { with: VALID_EMAIL_REGEX}, # メールアドレスの有効性
                     uniqueness: true # 一意性
+  validates :department, length: { in: 3..50 }, allow_blank: true
+  validates :basic_time, presence: true
+  validates :work_time, presence: true
+  
   has_secure_password # ハッシュ化しデータベースに保存
                       #has_secure_passwordがオブジェクト生成時に存在性を検証
   
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+ 
+  # 3文字以上かつ50文字以下,nil以外に空文字""にも対応
+
 end
