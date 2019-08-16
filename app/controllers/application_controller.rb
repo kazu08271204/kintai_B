97fd2d788a9ce3f936c004caa0ddcb56da3495df
kazu08271204 <jddpd083@gmail.com>
2019-08-16 03:23:14 +0000

    月日を一覧表示しよう

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper #どのコントローラでもヘルパーに定義したメソッドが使える
+
+  $days_of_the_week = %w{日 月 火 水 木 金 土}
+
 end
+
