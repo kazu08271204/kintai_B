f355777886945bf2fe817a2855f8cbfd7f6234f8
kazu08271204 <jddpd083@gmail.com>
2019-08-19 14:51:37 +0000

    出勤ボタン作成実装済み

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,18 @@
+class AttendancesController < ApplicationController
+  
+  def update
+    @user = User.find(params[:user_id])
+    @attendance = Attendance.find(params[:id])
+    
+    #出勤時間が未登録であることを判定します
+    if @attendance.started_at.nil?
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
+                                                                #何時何分までを表示(秒数を0に変換)            
+        flash[:info] = "おはようございます。"
+      else
+        flash[:denger] = "勤怠登録に失敗しました。やり直して下さい。"
+      end
+    end
+    redirect_to @user
+  end
+end
